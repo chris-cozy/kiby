d521a9fcfec112237882b895875b58102b8abbe5
chris-cozy <csande9@clemson.edu>
2023-04-11 12:15:10 -0400

    handled kirby response language

---
+++ src/events/messageCreate/response.js
@@ -0,0 +1,77 @@
+const { EmbedBuilder } = require('discord.js');
+/**
+ * Handle a message sent in the server. 
+ * Upon being mentioned, respond with random combination of kirby language
+ * @param {*} client - The bot
+ * @param {object} message - The message which was sent
+ */
+module.exports = async (client, message) => {
+    // Ignore msg if author is a bot
+    if (message.author.bot) {
+        return;
+    }
+
+    // Send alert if not sent in the right channel
+    if (message.channel.id !== process.env.OPENAI_CHANNEL_ID) {
+        console.log('not the designated channel');
+    }
+
+    // Ignore message if bot is not mentioned
+    if (!message.mentions.has(client.user.id)) {
+        return;
+    }
+
+    // Give the illusion of the bot typing
+    await message.channel.sendTyping();
+
+    const maxWordLength = 3;
+    const maxSentenceLength = 3;
+    const minWordLength = 1;
+    const minSentenceLength = 1;
+    const lexigraph = [
+        'yo',
+        'oy',
+        'pu',
+        'pa',
+        'ga',
+        'bu',
+        'lo',
+        'la',
+        'ha',
+        'ya',
+        'by',
+        'wu',
+    ];
+
+    const punctuation = [
+        '?',
+        '!',
+        '.',
+        '',
+    ]
+
+    let response = '';
+
+    while (response == '') {
+        // Return random length between 0 and the maximum word length for sentence
+        const sentenceLength = Math.floor(Math.random() * (maxSentenceLength - minSentenceLength + 1)) + minSentenceLength;
+
+        // Cycle through and construct each word
+        for (let i = 0; i < sentenceLength; i++) {
+
+            const wordLength = Math.floor(Math.random() * (maxWordLength - minWordLength + 1)) + minWordLength;
+
+            for (let k = 0; k < wordLength; k++) {
+                const index = Math.floor(Math.random() * lexigraph.length);
+                response += lexigraph[index];
+            }
+
+            response += ' ';
+        }
+
+        const punctuationIndex = Math.floor(Math.random() * punctuation.length);
+        response += punctuation[punctuationIndex];
+    }
+
+    message.reply(response);
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * @author Cozy
- * @version 1.0.0
+ * @version 1.1.0
  * @link discord.js.org/#/
  */
 const dotenv = require('dotenv');
