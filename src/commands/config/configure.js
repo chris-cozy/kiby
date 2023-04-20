2306c33eee4e7e9e7b6174756316376591db2f83
chris-cozy <csande9@clemson.edu>
2023-04-20 12:33:13 -0400

    setup configuration command

---
+++ src/commands/config/configure.js
@@ -0,0 +1,51 @@
+const { Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
+const autoRole = require('../../schemas/stats');
+
+module.exports = {
+    name: 'configure',
+    description: "Configure your kirby!",
+    options: [
+        {
+            name: 'name',
+            description: "Choose your kirby's name.",
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        }
+    ],
+
+    /**
+     * @brief Allow user to set up their kirby
+     * @param {Client} client 
+     * @param {Interaction} interaction 
+     */
+    callback: async (client, interaction) => {
+        await interaction.deferReply({ ephemeral: true });
+
+        if (!interaction.inGuild()) {
+            interaction.editReply("You can only run this command inside of a server.");
+            return;
+        }
+
+        const targetName = interaction.options.get('name').value;
+
+        // If user has a kirby, exit. If not, create it
+        try {
+            let userKirby = await userStats.findOne({ userId: interaction.user.id });
+
+            if (userKirby) {
+                interaction.editReply("You already have a kirby to take care of!");
+                return;
+            } else {
+                userKirby = new userStats({
+                    userId: interaction.user.id,
+                    kirbyName: targetName,
+                });
+            }
+
+            await userKirby.save();
+            interaction.editReply(`You now own a kirby! ${targetName} is a nice name for them.`);
+        } catch (error) {
+            console.log(`There was an error: $${error}`);
+        }
+    }
+}
