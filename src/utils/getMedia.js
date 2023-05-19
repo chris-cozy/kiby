cc6f801ece7468405be9ad6d3cfa105589eb277f
chris-cozy <csande9@clemson.edu>
2023-05-18 23:29:42 -0400

    switched from database search to local file search

--- src/utils/getMedia.js
+++ src/utils/getMedia.js
@@ -1,5 +1,7 @@
 const media = require('../schemas/media');
-const randomNumber = require('./randomNumber');
+const random_number = require('./randomNumber');
+const path = require('path');
+const get_all_files = require('./getAllFiles');
 
 /**
  * @brief Grab a media based on the request
@@ -9,9 +11,35 @@ const randomNumber = require('./randomNumber');
 module.exports = async (request) => {
     const keyword = request;
 
-    let allMedia = await media.find({ type: keyword });
+    let mediaSelection = []
 
-    const chosen = allMedia[randomNumber(0, (allMedia.length - 1))];
+    // Grab all subdirs of media directory
+    const mediaCategories = get_all_files(
+        path.join(__dirname, '..', 'media'),
+        true
+    )
+    console.log(mediaCategories);
 
-    return chosen;
+    for (const mediaCategory of mediaCategories) {
+        if (mediaCategory.substring(mediaCategory.lastIndexOf("\\") + 1) == keyword) {
+            const mediaFiles = get_all_files(mediaCategory);
+
+            for (const mediaFile of mediaFiles) {
+                mediaSelection.push(mediaFile);
+            }
+        } else {
+            continue;
+        }
+
+    }
+    console.log(mediaSelection);
+
+    const chosen = mediaSelection[random_number(0, (mediaSelection.length - 1))];
+
+    const media = {
+        name: chosen.substring(chosen.lastIndexOf("\\") + 1),
+        url: chosen,
+    }
+    console.log(media);
+    return media;
 }
