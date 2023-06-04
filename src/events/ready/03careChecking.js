4aba98da996d11e694db42f1c0d547fcb289df03
chris-cozy <csande9@clemson.edu>
2023-06-03 22:59:54 -0400

    Implemented hunger notification

--- src/events/ready/03careChecking.js
+++ src/events/ready/03careChecking.js
@@ -1,12 +1,14 @@
 const { ActivityType, Client } = require('discord.js');
 const userDates = require('../../schemas/dates');
 const userStats = require('../../schemas/stats');
-const randomNumber = require("../../utils/randomNumber");
+const random_number = require("../../utils/randomNumber");
+const hunger_notification = require("../../utils/hungerNotification");
 
 /**
  * @brief Periodically check the user's last care dates for kirby
+ * @param {Client} client
  */
-module.exports = () => {
+module.exports = (client) => {
 
     const milliConversion = 60000;
     const timer = 120 * milliConversion
@@ -35,15 +37,18 @@ module.exports = () => {
 
                 // Decrease hunger
                 if ((currentDate - userDate.lastFeed) > timer) {
-                    user.hunger -= randomNumber(10, 30);
+                    user.hunger -= random_number(10, 30);
                     if (user.hunger < min) {
                         user.hunger = min;
+                    }
+                    if (user.hunger < 50) {
+                        hunger_notification(user);
                     }
                 }
 
                 // Decrease affection
                 if (((currentDate - userDate.lastPet) > timer) || ((currentDate - userDate.lastPlay) > timer)) {
-                    user.affection -= randomNumber(10, 30);
+                    user.affection -= random_number(10, 30);
                     if (user.affection < min) {
                         user.affection = min;
                     }
@@ -51,7 +56,7 @@ module.exports = () => {
 
                 // Health decrease
                 if ((user.affection == min) || (user.hunger == min)) {
-                    user.hp -= randomNumber(10, 20);
+                    user.hp -= random_number(10, 20);
                     if (user.hp < min) {
                         user.hp = min;
                     }
@@ -59,7 +64,7 @@ module.exports = () => {
 
                 // Health increase
                 if ((user.affection == max) && (user.hunger == max)) {
-                    user.hp += randomNumber(10, 20);
+                    user.hp += random_number(10, 20);
                     if (user.hp > max) {
                         user.hp = max;
                     }
---
+++ src/utils/hungerNotification.js
@@ -0,0 +1,38 @@
+const { Client } = require('discord.js');
+const userStats = require('../../schemas/stats');
+const command = require('../classes/command');
+
+/**
+ * @brief Send user a notification that the kirby is hungry
+ * @param {Client} client
+ * @param {userStats} userStats
+ * @return N/A
+ */
+module.exports = async (client, userStats) => {
+    const hungerNotification = new command();
+    const media = await hungerNotification.get_media_attachment('hungry');
+    const user = client.users.cache.get(userStats.userId);
+
+    if (user) {
+        const embed = new EmbedBuilder()
+            .setTitle('**HUNGRY**')
+            .setColor(hungerNotification.pink)
+            .setDescription(`**${userStats.kirbyName}** is hungry!`)
+            .setImage(media.mediaString)
+            .setTimestamp()
+            .setFooter({ text: `${client.user.tag} `, iconURL: `${client.user.displayAvatarURL()}` });
+
+        user.send(embed)
+            .then(() => {
+                console.log('DM with embed sent successfully!');
+            })
+            .catch((error) => {
+                console.error('Error sending DM with embed:', error);
+            });
+    } else {
+        console.error('User not found!');
+    }
+
+
+
+}
