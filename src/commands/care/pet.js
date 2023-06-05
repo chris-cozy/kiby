1f863c16485c43cf94de565fdee2e9021dcfe2b4
chris-cozy <csande9@clemson.edu>
2023-06-04 23:57:37 -0400

    Optimized pet module

--- src/commands/care/pet.js
+++ src/commands/care/pet.js
@@ -1,8 +1,8 @@
 const { Client, Interaction, EmbedBuilder } = require('discord.js');
 const userStats = require('../../schemas/stats');
 const userDates = require('../../schemas/dates');
-const calculateXpForLevel = require('../../utils/calculateXpForLevel');
-const randomNumber = require("../../utils/randomNumber");
+const calculate_xp_for_level = require('../../utils/calculateXpForLevel');
+const random_number = require("../../utils/randomNumber");
 const command = require('../../classes/command');
 
 module.exports = {
@@ -13,7 +13,7 @@ module.exports = {
     deleted: false,
 
     /**
-     * @brief Pet user's kirby
+     * @brief Pet user's Kirby
      * @param {Client} client 
      * @param {Interaction} interaction 
      */
@@ -21,90 +21,86 @@ module.exports = {
         const pet = new command(5);
         let media = await pet.get_media_attachment('play');
 
-        if (interaction.inGuild()) {
-            await interaction.deferReply({ ephemeral: true });
-        } else {
-            await interaction.deferReply({ ephemeral: false });
+        const deferOptions = { ephemeral: !interaction.inGuild() };
+        await interaction.deferReply(deferOptions);
+
+        const userKirby = await userStats.findOne({ userId: interaction.user.id });
+        const userDate = await userDates.findOne({ userId: interaction.user.id });
+
+        // Check if user owns a Kirby
+        if (!userKirby) {
+            interaction.editReply("You don't yet own a Kirby! Use command **/adopt** to start your Kirby journey.");
+            return;
         }
 
-        let userKirby = await userStats.findOne({ userId: interaction.user.id });
-        let userDate = await userDates.findOne({ userId: interaction.user.id });
 
+        try {
+            const awakeDate = new Date(userDate.lastSleep.getTime() + pet.sleepTime);
 
-        // Check if user owns a kirby
-        if (userKirby) {
-            try {
-
-                const awakeDate = new Date(userDate.lastSleep.getTime() + pet.sleepTime);
-                // If Kirby is still asleep, alter image
-                if (pet.currentDate < awakeDate) {
-                    media = await pet.get_media_attachment('sleep');
-                }
-
-
-                // Check if it has been minimum time since last affection
-                if ((pet.currentDate - userDate.lastPet) < (pet.interactionCooldown)) {
-                    interaction.editReply({ content: `You can only pet ${userKirby.kirbyName} every ${pet.cooldownMins} minutes! They need personal space too!`, ephemeral: true });
-                    return;
-                }
-
-                // Generate affection and xp amount
-                let affectionGranted = randomNumber(2, 7);
-                const xpGranted = randomNumber(5, 15);
-
-                if ((userKirby.affection + affectionGranted) > pet.max) {
-                    affectionGranted = pet.max - userKirby.affection;
-                }
-
-                const embed = new EmbedBuilder()
-                    .setTitle('**PETTING**')
-                    .setColor(pet.pink)
-                    .setDescription(`**${interaction.user.username}** is petting **${userKirby.kirbyName}**!`)
-                    .addFields(
-                        {
-                            name: 'Health',
-                            value: `${userKirby.hp}`,
-                            inline: true
-                        },
-                        {
-                            name: 'Affection',
-                            value: `${userKirby.affection} -> ${userKirby.affection + affectionGranted}`,
-                            inline: true
-                        }
-                    )
-                    .setImage(media.mediaString)
-                    .setTimestamp()
-                    .setFooter({ text: `${client.user.tag} `, iconURL: `${client.user.displayAvatarURL()}` });
-
-                // Update feed and xp in db
-                userDate.lastPet = pet.currentDate;
-                userKirby.affection += affectionGranted;
-
-                userKirby.xp += xpGranted;
-                // If user's xp exceeds that for current level
-                if (userKirby.xp > calculateXpForLevel(userKirby.level)) {
-                    userKirby.xp = 0;
-                    userKirby.level += 1;
-
-                    interaction.editReply({ content: `**${userKirby.kirbyName}** has leveled up to level **${userKirby.level}**!` });
-                }
-
-                // Save database updates
-                await userKirby.save().catch((e) => {
-                    console.log(`There was an error saving: ${e}`);
-                });
-
-                await userDate.save().catch((e) => {
-                    console.log(`There was an error saving: ${e}`);
-                });
-
-                interaction.editReply({ embeds: [embed], files: [media.mediaAttach] });
-            } catch (error) {
-                console.log(`there was an error: ${error}`);
+            // If Kirby is still asleep, alter image
+            if (pet.currentDate < awakeDate) {
+                media = await pet.get_media_attachment('sleep');
             }
-        } else {
-            interaction.editReply(`You don't yet own a Kirby! Use command **/adopt** to start your Kirby journey.`);
-            return;
+
+
+            // Check if it has been enough time since last pet
+            if ((pet.currentDate - userDate.lastPet) < (pet.interactionCooldown)) {
+                interaction.editReply({
+                    content: `You can only pet ${userKirby.kirbyName} every ${pet.cooldownMins} minutes! They need personal space too!`,
+                    ephemeral: true,
+                });
+                return;
+            }
+
+            // Generate affection and XP amount
+            let affectionGranted = random_number(2, 7);
+            const xpGranted = random_number(5, 15);
+
+            if ((userKirby.affection + affectionGranted) > pet.max) {
+                affectionGranted = pet.max - userKirby.affection;
+            }
+
+            const embed = new EmbedBuilder()
+                .setTitle('**PETTING**')
+                .setColor(pet.pink)
+                .setDescription(`**${interaction.user.username}** is petting **${userKirby.kirbyName}**!`)
+                .addFields(
+                    {
+                        name: 'Health',
+                        value: `${userKirby.hp}`,
+                        inline: true
+                    },
+                    {
+                        name: 'Affection',
+                        value: `${userKirby.affection} -> ${userKirby.affection + affectionGranted}`,
+                        inline: true
+                    }
+                )
+                .setImage(media.mediaString)
+                .setTimestamp()
+                .setFooter({ text: `${client.user.tag} `, iconURL: `${client.user.displayAvatarURL()}` });
+
+            // Update pet and XP in the database
+            userDate.lastPet = pet.currentDate;
+            userKirby.affection += affectionGranted;
+            userKirby.xp += xpGranted;
+
+            // If user's XP exceeds that for the current level
+            if (userKirby.xp > calculate_xp_for_level(userKirby.level)) {
+                userKirby.xp = 0;
+                userKirby.level += 1;
+
+                interaction.editReply({ content: `**${userKirby.kirbyName}** has leveled up to level **${userKirby.level}**!` });
+            }
+
+            // Save database updates
+            await Promise.all([userKirby.save(), userDate.save()]).catch((e) => {
+                console.log(`There was an error saving: ${e}`);
+            });
+
+            interaction.editReply({ embeds: [embed], files: [media.mediaAttach] });
+        } catch (error) {
+            console.log(`there was an error: ${error}`);
         }
     },
 }
