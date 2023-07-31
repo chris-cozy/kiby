5f130433eec3c67e8822b90cb094b7717a336d8e
chris-cozy <csande9@clemson.edu>
2023-07-31 00:48:15 -0400

    Debugged message response

--- src/events/messageCreate/response.js
+++ src/events/messageCreate/response.js
@@ -1,6 +1,6 @@
-const { Client, Message } = require('discord.js');
+const { Client, Message } = require("discord.js");
 const construct_sentence = require("../../utils/constructSentence");
-const userStats = require('../../schemas/stats');
+const userStats = require("../../schemas/stats");
 
 /**
  * @brief Handle a message sent in the server. Upon being mentioned, respond with random combination of kirby language
@@ -8,44 +8,52 @@ const userStats = require('../../schemas/stats');
  * @param {Message} message - The message which was sent
  */
 module.exports = async (client, message) => {
-    // Ignore message if author is a bot
-    if (message.author.bot) {
-        return;
+  // Ignore message if author is a bot
+  if (message.author.bot) {
+    return;
+  }
+
+  // Check if the message is sent in a server or DMs
+  const isDM = message.channel.type === "DM";
+
+  // Ignore message if the bot is not mentioned in a server or a DM
+  if (!isDM && !message.mentions.has(client.user.id)) {
+    return;
+  }
+
+  let userKirby;
+
+  if (isDM) {
+    userKirby = await userStats.findOne({ userId: message.author.id });
+
+    if (!userKirby) {
+      message.reply(
+        `You don't yet own a Kirby! Use command **/adopt** to start your Kirby journey.`
+      );
+      return;
     }
+    console.log("DM");
+  } else {
+    userKirby = await userStats.findOne({ userId: message.author.id });
 
-    // Check if the message is sent in a server or DMs
-    const isDM = message.channel.type === 'DM';
-
-    // Ignore message if the bot is not mentioned in a server or a DM
-    if (!isDM && !message.mentions.has(client.user.id)) {
-        return;
+    if (!userKirby) {
+      message.reply(
+        `You don't yet own a Kirby! Use command **/adopt** to start your Kirby journey.`
+      );
+      return;
     }
+  }
 
-    let userKirby;
+  const kirbyName = userKirby.kirbyName;
 
-    if (isDM) {
-        userKirby = await userStats.findOne({ userId: message.author.id });
-
-        if (!userKirby) {
-            message.reply(`You don't yet own a Kirby! Use command **/adopt** to start your Kirby journey.`);
-            return;
-        }
-        console.log('DM');
-    } else {
-        userKirby = await userStats.findOne({ userId: message.author.id });
-
-        if (!userKirby) {
-            message.reply(`You don't yet own a Kirby! Use command **/adopt** to start your Kirby journey.`);
-            return;
-        }
-    }
-
-    const kirbyName = userKirby.kirbyName;
-
-    // Give the illusion of bot typing
+  // Give the illusion of bot typing
+  try {
     await message.channel.sendTyping();
 
     const response = construct_sentence();
 
     message.reply(`**${kirbyName}**: ` + response);
+  } catch (error) {
+    print(`Could not send Kirby message reply due to error: ${error}`);
+  }
 };
